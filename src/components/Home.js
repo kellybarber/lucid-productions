4141401363e4c00305cb12131c6804d74d65be0d
kellybarber <kelly.james.barber@gmail.com>
2017-11-28 19:21:20 -0500

    implementing a css grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,8 @@ import React from 'react'
 class Home extends React.Component {
   render() {
     return (
-      <div className='HomeWrapper'>
-        <iframe title="main-media" width="560" height="315" src="https://www.youtube.com/embed/dL-8y0ph5kw" frameborder="0" allowfullscreen></iframe>
+      <div className='wrapper'>
+        <iframe className="video" title="main-media" src="https://www.youtube.com/embed/dL-8y0ph5kw" frameBorder="0" allowFullScreen></iframe>
 
 
       </div>
