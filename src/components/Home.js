cc1e6ccf49fff677fde51d1435efd84488d39070
kellybarber <kelly.james.barber@gmail.com>
2017-11-28 20:41:37 -0500

    made iframe cooperate with grid layout and maintain aspect ratio

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,9 @@ class Home extends React.Component {
   render() {
     return (
       <div className='wrapper'>
-        <iframe className="video" title="main-media" src="https://www.youtube.com/embed/dL-8y0ph5kw" frameBorder="0" allowFullScreen></iframe>
-
+        <div className='video-container'>
+          <iframe className="video" title="main-media" src="https://www.youtube.com/embed/dL-8y0ph5kw" frameBorder="0" allowFullScreen></iframe>
+        </div>
 
       </div>
     )
