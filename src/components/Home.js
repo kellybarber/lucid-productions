0bd2577f610649036f4d14d1f85bb56e12ba4803
kellybarber <kelly.james.barber@gmail.com>
2017-11-29 12:18:53 -0500

    working om donate button

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends React.Component {
         <div className='video-container'>
           <iframe className="video" title="main-media" src="https://www.youtube.com/embed/dL-8y0ph5kw" frameBorder="0" allowFullScreen></iframe>
         </div>
-
+        <div className='donate'></div>
       </div>
     )
   }
